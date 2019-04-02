a7b8362badfe239f3190b4b366d7949176feb8ea
likonion <likonion@outlook.com>
2019-04-02 14:55:28 +0800

    build

---
+++ todolist/build/precache-manifest.d3cec42e00751dc032b029edc3f372ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "c5940d94e014861adda8",
+    "url": "./static/js/main.0c62dd7a.chunk.js"
+  },
+  {
+    "revision": "08facb8ebb9b39a39bd1",
+    "url": "./static/js/2.78a01fad.chunk.js"
+  },
+  {
+    "revision": "c5940d94e014861adda8",
+    "url": "./static/css/main.50b124ed.chunk.css"
+  },
+  {
+    "revision": "a723f9e462da2b784388e9a909130a01",
+    "url": "./index.html"
+  }
+];
--- todolist/build/service-worker.js
+++ todolist/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-demo/precache-manifest.1b40419ecf4fdd42e2fa88464c8e4c0f.js"
+  "./precache-manifest.d3cec42e00751dc032b029edc3f372ae.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-demo/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
