823a2ae24732e8f4e3265ce63da1427571e73182
likonion <likonion@outlook.com>
2019-04-02 14:58:34 +0800

    build

---
+++ my-app/build/precache-manifest.400efae4ce3c84c75a795d60c2d3d926.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "1d3e19ad021dacbbd087",
+    "url": "./static/js/main.0416cd9d.chunk.js"
+  },
+  {
+    "revision": "458a762c275fcd412125",
+    "url": "./static/js/2.4e735392.chunk.js"
+  },
+  {
+    "revision": "1d3e19ad021dacbbd087",
+    "url": "./static/css/main.24bbc4e4.chunk.css"
+  },
+  {
+    "revision": "458a762c275fcd412125",
+    "url": "./static/css/2.ab86fefd.chunk.css"
+  },
+  {
+    "revision": "c9530290a3f70f93cfe1481e790118a1",
+    "url": "./index.html"
+  }
+];
